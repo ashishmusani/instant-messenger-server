76da4dfea7119b23ec71d35b949d34432b6932fc
ashishmusani <ashish_musani@ymail.com>
2020-09-14 12:44:23 +0530

    added procfile

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const cors = require('cors')
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
